805a9fee778054e37fcc29cb1c377add87a621c4
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-02-15 00:49:01 +0700

    Java Core - Contructor co Tham So

---
+++ src/JavaCore/OOP/VD30/mainMyDate.java
@@ -0,0 +1,11 @@
+package JavaCore.OOP.VD30;
+
+public class mainMyDate {
+    public static void main(String[] args) {
+        myDate md = new myDate(15, 2, 2025);
+        md.printDate();
+        md.printDay();
+        md.printMonth();
+        md.printYear();
+    }
+}
