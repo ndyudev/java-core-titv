a7655ea8e49ef218aba28acd15f2ff69da9c9709
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-02-19 07:25:03 +0700

    Video 34 TiTV Equals HashCode

---
+++ src/JavaCore/OOP/VD34/main_equals.java
@@ -0,0 +1,12 @@
+package JavaCore.OOP.VD34;
+
+import java.util.Scanner;
+
+public class main_equals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        equals eq = new equals("", "Chau","");
+        System.out.println("Nhap ho va ten:");
+        eq.xuat();
+    }
+}
