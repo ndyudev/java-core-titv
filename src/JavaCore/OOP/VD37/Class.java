84e3c33024553de8499f0d412427739e2d47acc5
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-04-02 22:51:15 +0700

    Student Manager 23

--- src/JavaCore/OOP/VD37/Class.java
+++ src/JavaCore/OOP/VD37/Class.java
@@ -5,13 +5,5 @@ public class Class {
     private String idStudent;
     private double poin;
     private int namSinh;
-    private int idClass;
-    private String maMon;
-    private String ngayBatDau;
-    private String ngatKetThuc;
-    private String diaChiGiaoVien;
-    private String tenGianGVien;
-    private String nhanVien;
-    private String maNhanVien;
-    private String nhanVien2;
+    private double age;
 }
