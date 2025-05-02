1c9596e61a400a8c269fb7f69ffa41eec6bea003
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-05-02 23:26:36 +0700

    Student Manager 46

--- src/JavaCore/OOP/VD37/Class.java
+++ src/JavaCore/OOP/VD37/Class.java
@@ -2,13 +2,9 @@ package JavaCore.OOP.VD37;
 
 public class Class {
     private String nameStudent;
-    private String sourseIdAddress;
-    private String sourseName;
-    private String className;
-    private String sourseAddress;
-    private String classAddress;
     private String sourseId;
     private String classId;
     private String classType;
     private String sourseType;
+    private String className;
 }
