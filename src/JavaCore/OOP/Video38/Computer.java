b2c91f3a4402fbe46181328a715b1a402a38e67d
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-05-10 21:17:24 +0700

    Computer Manage Video 38 TiTv 3

--- src/JavaCore/OOP/Video38/Computer.java
+++ src/JavaCore/OOP/Video38/Computer.java
@@ -5,4 +5,13 @@ public class Computer {
     private Double price;
     private Manufacturer manufacturer;
     private String timeGuarantee;
+
+    public Computer() {}
+
+    public Computer(Date dateProduction, Double price, Manufacturer manufacturer, String timeGuarantee) {
+        this.dateProduction = dateProduction;
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.timeGuarantee = timeGuarantee;
+    }
 }
