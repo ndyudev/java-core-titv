6b90266c99d09e36954f7259f39685be7e87f57f
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-05-18 14:11:55 +0700

    Overloading Video 43 TiTV

---
+++ src/JavaCore/Overloading/MathOverloading.java
@@ -0,0 +1,16 @@
+package JavaCore.Overloading;
+/*
+Overloading (Nạp chồng phương thức) nghĩa là trong cùng một class
+bạn có nhiều phương thức cùng tên nhưng khác nhau về số lượng tham số
+hoặc kiểu dữ liệu của tham số.
+ */
+public class MathOverloading {
+    public static void main(String[] args) {
+        MyMath mymath = new MyMath();
+        System.out.println(mymath.timMin(5, 6));
+        System.out.println(mymath.timMin(5.5, 6.5));
+        System.out.println(mymath.tinhTong(5,6));
+        double arr[] = new double[] {1,2,3,4,5};
+        System.out.println(mymath.tinhTong(arr, 0));
+    }
+}
---
+++ src/JavaCore/Overloading/MyMath.java
@@ -0,0 +1,32 @@
+package JavaCore.Overloading;
+
+public class MyMath {
+
+    public int timMin(int a, int b) {
+        if( a > b ) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public double timMin( double a, double b ) {
+        if( a > b ) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public int tinhTong(int a, int b ) {
+        return a + b;
+    }
+
+    public double tinhTong(double[] arr, double a) {
+        double tong = 0;
+        for (int i = 0; i < arr.length; i++) {
+            tong += arr[i];
+        }
+        return tong;
+    }
+}
