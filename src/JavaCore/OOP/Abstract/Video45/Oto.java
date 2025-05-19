1bfc04de542f016bb758c7c6759c9c7db3b98cf4
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-05-19 22:11:48 +0700

    Abstract Inheritance Video 45 TiTV

---
+++ src/JavaCore/OOP/Abstract/Video45/Oto.java
@@ -0,0 +1,20 @@
+package JavaCore.OOP.Abstract.Video45;
+
+public class Oto extends PhuongTienDiChuyen{
+    private String loaiNhienLieu;
+
+    public Oto(String tenLoaiPhuongTien, String loaiNhienLieu) {
+        super(tenLoaiPhuongTien);
+        this.loaiNhienLieu = loaiNhienLieu;
+    }
+
+    public String getLoaiNhienLieu() {
+        return loaiNhienLieu;
+    }
+
+    public void setLoaiNhienLieu(String loaiNhienLieu) {
+        this.loaiNhienLieu = loaiNhienLieu;
+    }
+
+    @Override
+}
---
+++ src/JavaCore/OOP/Abstract/Video45/PhuongTienDiChuyen.java
@@ -0,0 +1,24 @@
+package JavaCore.OOP.Abstract.Video45;
+
+public abstract class PhuongTienDiChuyen {
+    protected String tenLoaiPhuongTien;
+
+    public void batDau() {
+        System.out.println("Bắt đầu");
+    }
+
+    public void tangToc() {
+        System.out.println("Tăng Tốc");
+    }
+
+    public PhuongTienDiChuyen(String tenLoaiPhuongTien) {
+        this.tenLoaiPhuongTien = tenLoaiPhuongTien;
+    }
+
+    public void dungLai() {
+        System.out.println("Dừng Lại");
+    }
+
+    public abstract void layVanToc();
+
+}
