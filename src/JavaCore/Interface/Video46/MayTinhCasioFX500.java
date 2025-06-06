13c707cc497f3524bbdbd001f2417f7115b66a5c
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-06-06 22:33:11 +0700

    Interface Video 46 TiTV 2

--- src/JavaCore/Interface/Video46/MayTinhCasioFX500.java
+++ src/JavaCore/Interface/Video46/MayTinhCasioFX500.java
@@ -2,7 +2,20 @@ package JavaCore.Interface.Video46;
 
 public class MayTinhCasioFX500 implements MayTinhBoTuiInterface {
 
+    @Override
+    public double Chia(double a, double b) {
+        return Cong(a, b);
+    }
 
+    @Override
+    public double Nhan(double a, double b) {
+        return Cong(a, b);
+    }
+
+    @Override
+    public double Tru(double a, double b) {
+        return Cong(a, b);
+    }
 
     @Override
     public double Cong(double a, double b) {
