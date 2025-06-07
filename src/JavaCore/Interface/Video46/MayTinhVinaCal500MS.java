bd935d0ccbc92c233568eef915254ebbc6385965
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-06-07 16:13:12 +0700

    Interface Video 46 TiTV 3

--- src/JavaCore/Interface/Video46/MayTinhVinaCal500MS.java
+++ src/JavaCore/Interface/Video46/MayTinhVinaCal500MS.java
@@ -1,4 +1,24 @@
 package JavaCore.Interface.Video46;
 
-public class MayTinhVinaCal500MS {
+public class MayTinhVinaCal500MS implements MayTinhBoTuiInterface {
+
+    @Override
+    public double Chia(double a, double b) {
+        return Cong(a, b);
+    }
+
+    @Override
+    public double Nhan(double a, double b) {
+        return Cong(a, b);
+    }
+
+    @Override
+    public double Tru(double a, double b) {
+        return Cong(a, b);
+    }
+
+    @Override
+    public double Cong(double a, double b) {
+        return Cong(a, b);
+    }
 }
