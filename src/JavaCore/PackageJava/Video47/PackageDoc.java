f84a41d87a0345fccde80ea51191569cfacf93b4
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-07-07 20:34:20 +0700

    Video 47 - TiTv - Pakage

--- src/JavaCore/PackageJava/Video47/PackageDoc.java
+++ src/JavaCore/PackageJava/Video47/PackageDoc.java
@@ -1,5 +1,9 @@
 package JavaCore.PackageJava.Video47;
+/*
+package trong Java là một nhóm các class, interface, enum, ... được tổ chức cùng nhau trong một thư mục có tên cụ thể.
 
+Nó giúp quản lý mã nguồn gọn gàng, tránh trùng tên và phân chia chức năng rõ ràng.
+ */
 public class PackageDoc {
 
 }
