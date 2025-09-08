6e7de36376822dc5ba56d014b0f8de6e085b3031
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-09-08 21:43:06 +0700

    Find string method java core

---
+++ src/JavaCore/String/Video51/IndexString.java
@@ -0,0 +1,19 @@
+package JavaCore.String.Video51;
+
+public class IndexString {
+    public static void main(String[] args) {
+        String s1 = "Hello World";
+        String exp = "Ví dụ";
+        String hello = "World";
+        System.out.println("Vị trí:" + s1.indexOf(hello)); // Sử dụng vị trí bắt đầu
+
+        System.out.println("Vị trí:" + s1.indexOf(hello, 5)); // Nêu ra vị trí bắt đầu
+
+
+        // Tìm kiếm char
+
+
+
+    }
+}
+
